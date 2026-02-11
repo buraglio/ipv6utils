058e650cd93287d34da258fa95162b364302d643
Nick Buraglio <buraglio@forwardingplane.net>
2026-02-11 13:07:29 -0600

    Add version information to ipv6utils formula

--- Formula/ipv6utils.rb
+++ Formula/ipv6utils.rb
@@ -2,7 +2,8 @@ class Ipv6utils < Formula
   desc "A toolset for IPv6 utilities"
   homepage "https://github.com/buraglio/ipv6utils"
   url "https://github.com/buraglio/ipv6utils/archive/refs/tags/v4.tar.gz"
-  sha256 "604c50eccc07db6c96b7efefbeb1e6b853e9681e0639297e119e3f34be367dd4"  
+  sha256 "604c50eccc07db6c96b7efefbeb1e6b853e9681e0639297e119e3f34be367dd4"
+  version "4"
   depends_on "go"  #  Ensures Go is installed to build the project
 
   def install
